456ccd4853ea7ec1da6dbff9c136bccacbb9e700
steven huynh <steven.h@live.com>
2024-01-22 12:39:25 -0800

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css'
+import './App.css';
+
+import { puppyList } from './data';
+import { useState } from 'react';
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find(pup => pup.id === featPupId);
+
   return (
     <>
+      <div className='main'>
+        {
+          puppies.map(puppy => {
+            return <p key={puppy.id} onClick={() => {
+              setFeatPupId(puppy.id);
+            }}>{puppy.name}</p>;
+          })
+        }
+      </div>
+      {
+        featPupId && (
+          <div className='featured'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )
+      }
     </>
   )
 }
